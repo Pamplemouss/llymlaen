6cb387992407914a08b4120da8e89c952f2b2d39
Pamplemouss <john.dufaye@gmail.com>
2023-03-26 05:50:23 +0200

    Logo fix

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -6,7 +6,7 @@ export default function TopBar() {
         <div className="fixed top-0 left-0 px-5 flex h-16 w-full bg-gradient-to-b from-slate-700 to-slate-800 shadow-lg shadow-black/50 items-center z-50">
             <Link href="/" className="h-full flex justify-center items-center">
                 <div className="h-[85%] aspect-square relative">
-                    <Image alt="logo" fill src="/logo.avif" sizes="(max-width: 768px) 100vw,(max-width: 1200px) 50vw,33vw" className="object-contain"></Image>
+                    <Image alt="logo" fill src="/logo.png" sizes="(max-width: 768px) 100vw,(max-width: 1200px) 50vw,33vw" className="object-contain"></Image>
                 </div>
                 <div className="text-xl text-emerald-400 font-neosans tracking-wider flex justify-center items-center ml-5">EORGUESSR</div>
             </Link>
