061f869a4e2234baee2e00ed8d13b54844f6f3f5
Pamplemouss <john.dufaye@gmail.com>
2023-03-26 05:09:41 +0200

    Title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,7 +47,7 @@ export default function Home() {
     return (
         <>
             <Head>
-                <title>FFXIV Geoguessr</title>
+                <title>Eorguessr</title>
                 <link key="favApple" rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"></link>
                 <link key="fav32" rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"></link>
                 <link key="fav16" rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"></link>
