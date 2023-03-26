f48276d3217d87503c0f2d6489098071a3928b27
Pamplemouss <john.dufaye@gmail.com>
2023-03-26 06:43:18 +0200

    Analytics - 3rd try - God help me

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import '@/styles/globals.css'
 import React from 'react'
 import App from 'next/app'
 import {UserAgentProvider} from '@quentin-sommer/react-useragent'
+import Script from 'next/script'
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
 
 
 /* eslint-disable react/display-name */
@@ -34,7 +36,22 @@ class MyApp extends App {
   render() {
     const {Component, pageProps} = this.props
     return (
-      <Component {...pageProps} />
+      <>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}', {cookie_flags: 'SameSite=None;Secure'});
+          `}
+        </Script>
+        <Component {...pageProps} />
+      </>
+      
     )
   }
 }
