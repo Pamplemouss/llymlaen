7b06a19b4234c018f9ff988b6061eacc2d2ddd47
John <john.dufaye@gmail.com>
2024-05-07 05:54:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const next = require("next");
 const fs = require("fs");
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
+const port = process.env.PORT || 3000;
 const handle = app.getRequestHandler();
 const httpsOptions = {
   key: fs.readFileSync("./localhost.key"),
@@ -13,7 +14,7 @@ app.prepare().then(() => {
   createServer(httpsOptions, (req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen(3000, (err) => {
+  }).listen(port, (err) => {
     if (err) throw err;
     console.log("> Server started on https://localhost:3000");
   });
